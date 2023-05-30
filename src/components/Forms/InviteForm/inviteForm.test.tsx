406c01dad5fae0585b6ec4e9f2ba1a56a26dfec7
Ray Ranola <ray@beforepay.co>
2023-05-30 10:05:21 +1000

    fix: build - unused import in test

--- src/components/Forms/InviteForm/inviteForm.test.tsx
+++ src/components/Forms/InviteForm/inviteForm.test.tsx
@@ -1,8 +1,6 @@
 import { render, screen } from '../../../test/utils';
 import InviteForm from './InviteForm';
 
-import React from 'react';
-
 describe('Invite form', () => {
   it('should render', () => {
     render(<InviteForm onSuccess={() => void 0} />);
