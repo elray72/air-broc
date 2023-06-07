001f45329dbd8e023b69ed154d461fd2e2ed3c56
Ray Ranola <ray@beforepay.co>
2023-06-07 18:04:49 +1000

    feat: green bg

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
